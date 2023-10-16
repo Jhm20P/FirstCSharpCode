bb8b71c97cf702a48d11650875b5977809cf4359
Jonas Mikkelsen <jmikkelsen40@outlook.com>
2023-10-16 12:49:33 +0200

    Changed the console.writeline to console.write

--- Dag 1 - Consol/Program.cs
+++ Dag 1 - Consol/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Test for git commit");
+Console.Write("Hello, World!\n");
+Console.Write("Test for git commit\n");
